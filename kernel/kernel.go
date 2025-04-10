15979449f874527a587f5667e47bebd95ce7b2de
fedecotens <fcotens@frba.utn.edu.ar>
2025-04-10 15:51:27 +0000

    cree globals y el config y logger del kernel

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
+	"log/slog"
 
+	globals "github.com/sisoputnfrba/tp-golang/globals/kernel"
+	utils "github.com/sisoputnfrba/tp-golang/utils/kernel"
+)
+
+func main() {
+	utils.ConfigurarLogger()
+
+	globals.KernelConfig = utils.IniciarConfiguracion("config.json")
+	if globals.KernelConfig == nil {
+		log.Fatal("No se pudo iniciar el config")
+	}
+
+	slog.SetLogLoggerLevel(utils.Log_level_from_string(globals.KernelConfig.Log_level))
+
+	slog.Info(globals.KernelConfig.Ip_memory)
 }
