ef537a65f572ebf68f30999a77d595710d21affe
fedecotens <fcotens@frba.utn.edu.ar>
2025-04-13 15:32:27 +0000

    log level en IO

--- globals/io/globals.go
+++ globals/io/globals.go
@@ -4,7 +4,7 @@ type Io_Config struct {
 	IpKernel   string `json:"ip_kernel"`
 	PortKernel int64  `json:"port_kernel"`
 	PortIO     int64  `json:"port_io"`
-	LogLevel   int64  `json:"log_level"`
+	LogLevel   string `json:"log_level"`
 }
 
 var IoConfig *Io_Config
--- io/io.go
+++ io/io.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"log/slog"
 
 	globals "github.com/sisoputnfrba/tp-golang/globals/io"
 	utils_io "github.com/sisoputnfrba/tp-golang/io/utils"
@@ -15,5 +16,7 @@ func main() {
 	if globals.IoConfig == nil {
 		log.Fatal("No se pudo iniciar el config")
 	}
+
+	slog.SetLogLoggerLevel(utils_logger.Log_level_from_string(globals.IoConfig.LogLevel))
 
 }
