97ab974f56614549404b7d609f36e8341a448af0
fedecotens <fcotens@frba.utn.edu.ar>
2025-04-20 16:43:22 +0000

    Estructuras kernel

--- globals/kernel/globals.go
+++ globals/kernel/globals.go
@@ -22,3 +22,66 @@ type HandshakeIO struct {
 	IP     string `json:"ip"`
 	Puerto int    `json:"puerto"`
 }
+
+// Constantes
+const NEW string = "NEW"
+const READY string = "READY"
+const EXECUTE string = "EXECUTE"
+const BLOCKED string = "BLOCKED"
+const SUSP_BLOCKED string = "SUSP_BLOCKED"
+const SUSP_READY string = "SUSP_READY"
+const EXIT string = "EXIT"
+
+// Estructuras para manejo de procesos
+
+type Metricas struct {
+	New          int64
+	Ready        int64
+	Execute      int64
+	Blocked      int64
+	Susp_Ready   int64
+	Susp_Blocked int64
+}
+type PCB struct {
+	Pid int64
+	PC  int64
+	ME  Metricas
+	MT  Metricas
+}
+
+type Rafagas struct {
+	Est_Ant  int64
+	Raf_Ant  int64
+	Est_Sgte int64
+}
+
+type Proceso struct {
+	Pcb           PCB
+	Estado_Actual string
+	Rafaga        *Rafagas
+}
+
+type Proceso_Nuevo struct {
+	Archivo_Pseudocodigo string
+	Tamaño               int64
+	Proceso              Proceso
+}
+
+var MapaProcesos map[int64]Proceso = make(map[int64]Proceso)
+
+// Estructuras para los estados
+
+type Execute_Struct struct {
+	Libre bool
+	Pid   int64
+}
+type Estados struct {
+	NEW          []Proceso_Nuevo
+	READY        []int
+	EXECUTE      []Execute_Struct
+	BLOCKED      []int
+	SUSP_BLOCKED []int
+	SUSP_READY   []int
+}
+
+var ESTADOS Estados
