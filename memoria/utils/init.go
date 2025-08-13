8294aad3ccfadcc53a72457eca1372ee19adb14c
Juanma <jubravo@frba.utn.edu.ar>
2025-08-13 15:55:57 -0300

    Update init.go

--- memoria/utils/init.go
+++ memoria/utils/init.go
@@ -30,29 +30,6 @@ func IniciarConfiguracion(filePath string) *globals_memoria.Memoria_Config {
 	return config
 }
 
-/*
-func InicializarMemoria() {
-	tamanio := globals_memoria.MemoriaConfig.Memory_size
-	tamanioPagina := globals_memoria.MemoriaConfig.Page_size
-	cantidadMarcos := tamanio / tamanioPagina
-
-	globals_memoria.Memoria = make([]byte, tamanio)
-	globals_memoria.MemoriaMarcosOcupados = make([]bool, cantidadMarcos)
-
-	var manager globals_memoria.Manager = make(globals_memoria.Manager)
-	globals_memoria.Tablas = &manager
-
-	globals_memoria.Procesos = make(globals_memoria.ProcesosMap)
-
-	var metricasMap globals_memoria.MetricasMap = make(globals_memoria.MetricasMap)
-	globals_memoria.Metricas = &metricasMap
-	//SWAP
-
-	globals_memoria.ListaPaginasSwapDisponibles = make([]globals_memoria.Pagina, 0)
-
-	globals_memoria.ProximoIndiceSwap = 0
-}*/
-
 func InicializarMemoria() {
 	tamanio := globals_memoria.MemoriaConfig.Memory_size
 	tamanioPagina := globals_memoria.MemoriaConfig.Page_size
