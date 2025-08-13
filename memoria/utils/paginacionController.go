5b4f5ce7266f4fd9ae57ccd90af8af91d5354052
Juanma <jubravo@frba.utn.edu.ar>
2025-08-13 16:00:13 -0300

    Saco comentarioss

--- memoria/utils/paginacionController.go
+++ memoria/utils/paginacionController.go
@@ -5,84 +5,9 @@ import (
 	"fmt"
 	"log/slog"
 	"time"
-
-	//"fmt"
-	//"bufio"
-
 	"net/http"
-
-	//"os"
-	//"strings"
-
-	//globals "github.com/sisoputnfrba/tp-golang/globals/memoria"
 	globals_memoria "github.com/sisoputnfrba/tp-golang/globals/memoria"
 )
-
-/*
-func ActualizarTablaDePaginas(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var mensaje globals_memoria.TablaDTO
-	err := decoder.Decode(&mensaje)
-	if err != nil {
-		log.Printf("Error al decodificar mensaje: %s\n", err.Error())
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Error al decodificar mensaje"))
-		return
-	}
-	var delayMem int64 = globals_memoria.MemoriaConfig.Memory_delay
-
-	time.Sleep(time.Duration(delayMem) * time.Millisecond)
-	actualizarTablaPaginas(int(mensaje.Pid), mensaje.Indices)
-
-	log.Printf("Proceso %d actualizo tabla correctamente", mensaje.Pid)
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("ok"))
-}*/
-
-/*
-func ObtenerMarcoProceso(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var mensaje globals_memoria.ConsultaPaginaDTO
-	err := decoder.Decode(&mensaje)
-	if err != nil {
-		log.Printf("Error al decodificar mensaje: %s\n", err.Error())
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Error al decodificar mensaje"))
-		return
-	}
-
-	var pid int = int(mensaje.Pid)
-	var primerIndice int = int(mensaje.PrimerIndice)
-	var marco int
-	var delayMem int64 = globals_memoria.MemoriaConfig.Memory_delay
-
-	time.Sleep(time.Duration(delayMem) * time.Millisecond)
-	marco = obtenerMarcoDesdeTabla(pid, primerIndice)
-
-	if marco < 0 {
-		log.Printf("Error al obtener marco")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Error al obtener marco"))
-		return
-	}
-
-	var enviado struct {
-		Dato int `json:"dato"`
-	}
-	enviado.Dato = marco
-	jsonData, err := json.Marshal(enviado)
-	if err != nil {
-		log.Printf("Error al codificar la instruccion a JSON: %s", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Error interno del servidor"))
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonData)
-
-}*/
 
 func ObtenerMarcoProceso(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
